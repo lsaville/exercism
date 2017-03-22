b12f2704aa5f1b91fa348a30354dff397e33103c
Lee Saville <l.w.saville@gmail.com>
2017-03-22 14:04:17 -0600

    Point test at v4, try to implement suggestion of kotp

---
+++ ruby/raindrops/raindrops_v4.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class Raindrops
+  NUMBER_TO_WORD = {
+    3 => 'Pling',
+    5 => 'Plang',
+    7 => 'Plong'
+  }
+
+  def self.convert(number)
+    NUMBER_TO_WORD.each_with_object('') do |(test_number, rain_word), rain_speak|
+      rain_speak << rain_word if (number % test_number).zero?
+    end.empty? ? number.to_s : binding.pry
+#    rain_speak.empty? ? number.to_s : rain_speak
+  end
+end
+
+module BookKeeping
+  VERSION = 3
+end
